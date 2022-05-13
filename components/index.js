28ed3cfdeb1a39f8cd5c59f9c9894f33821b752f
global-Toba-dean <global-tobaogundimu@gmail.com>
2022-05-13 03:54:52 +0100

    my first react native app

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { SubInfo, EndDate, EthPrice, NFTTitle, People } from "./SubInfo";
+import { RectButton, CircleButton } from "./Buttons";
+import { DetailBid } from "./DetailBid";
+import { DetailsDesc } from "./DetailsDesc";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  EndDate,
+  EthPrice,
+  NFTTitle, 
+  People,
+  SubInfo,
+  DetailBid,
+  DetailsDesc
+}
